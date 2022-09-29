b4fc35558f0bf7a61b24048218db157557f76408
Maxim Therrien <maxim@acerspyro.ca>
2022-09-28 23:10:39 -0400

    FEAT - Generate html bundle for static serving

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.APP_ENV === "development" ? "/" : "/dist/",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    manifest: true,
-    minify: "terser",
-    rollupOptions: {
-      input: path.resolve(__dirname, "src/main.tsx"),
-    },
-  },
+  base: "./",
+  // base: process.env.APP_ENV === "development" ? "/" : "/dist/",
+  // build: {
+  //   outDir: "./dist",
+  //   emptyOutDir: true,
+  //   manifest: true,
+  //   minify: "terser",
+  //   rollupOptions: {
+  //     input: path.resolve(__dirname, "src/main.tsx"),
+  //   },
+  // },
   resolve: {
     alias: [
       {
